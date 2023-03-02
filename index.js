9d89db23eb21cbd9772817a9c078d4727213211c
mbakoG90210 <mbakogoitsemang@live.com>
2023-03-02 17:06:26 +0200

    Backend update to heroku build 1

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
+import path from 'path';
 import connectDB from './mongodb/connect.js';
 import postRoutes from './routes/postRoutes.js';
 import dalleRoutes from './routes/dalleRoutes.js';
 
 dotenv.config();
-
+const port = process.env.PORT || 8080;
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
@@ -14,16 +15,23 @@ app.use(express.json({ limit: "50mb" }));
 app.use('/api/v1/post', postRoutes);
 app.use('/api/v1/dalle', dalleRoutes);
 
-app.get('/', async (req, res) => {
+/* app.get('/', async (req, res) => {
   res.status(200).json({
-    message: 'Hello from DALL.E!',
+    message: 'Hello from DreamsAI Server! Welcome to the future 🚀',
   });
-});
+}); */
+if (process.env.NODE_ENV === 'production'){
 
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'))
+  });
+}
 const startServer = async () => {
     try {
       connectDB(process.env.MONGODB_URL);
-      app.listen(8080, () => console.log('Server started on port 8080'));
+      app.listen(port, () => console.log('Server started on port 8080 🚀'));
     } catch (error) {
       console.log(error);
     }
