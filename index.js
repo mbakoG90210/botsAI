a3c2980ea7b761921c056815240563fcaf50dc08
mbakoG90210 <mbakogoitsemang@live.com>
2023-03-04 00:28:12 +0200

    Heroku nodejs final build minus Procfile

--- index.js
+++ index.js
@@ -15,19 +15,12 @@ app.use(express.json({ limit: "50mb" }));
 app.use('/api/v1/post', postRoutes);
 app.use('/api/v1/dalle', dalleRoutes);
 
-/* app.get('/', async (req, res) => {
+ app.get('/', async (req, res) => {
   res.status(200).json({
     message: 'Hello from DreamsAI Server! Welcome to the future 🚀',
   });
-}); */
-if (process.env.NODE_ENV === 'production'){
+}); 
 
-  app.use(express.static(path.join(__dirname, 'client/dist')));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/dist/index.html'))
-  });
-}
 const startServer = async () => {
     try {
       connectDB(process.env.MONGODB_URL);
